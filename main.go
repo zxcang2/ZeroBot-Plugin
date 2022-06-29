190c436f621961ea3397de8e276462a7f0ae6e4b
zxcang2 <108434813+zxcang2@users.noreply.github.com>
2022-06-30 07:28:05 +0800

    调试

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ import (
 
 	// 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/manager" // 群管
 
-	// 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/thesaurus" // 词典匹配回复
+		_ "github.com/FloatTech/ZeroBot-Plugin/plugin/thesaurus" // 词典匹配回复
 
 	_ "github.com/FloatTech/zbputils/job" // 定时指令触发器
 
